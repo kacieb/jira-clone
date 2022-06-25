bc1866cfb4ac8c3b1cf1b7f3c8f33e6c746d4ce4
Kacie Bawiec <kaciebawiec@gmail.com>
2022-06-24 23:24:42 -0400

    Add modal support to NewTaskModal

--- src/NewTaskModal.tsx
+++ src/NewTaskModal.tsx
@@ -6,28 +6,57 @@ export default function NewTaskModal(props: {
   createTask: APIS["createTaskType"];
 }) {
   const [category, setCategory] = React.useState<Category>(Category.Backlog);
+  const [showModal, setShowModal] = React.useState<Boolean>(false);
 
   const onSubmit = (event) => {
     props.createTask(category);
     event.preventDefault(); // prevents page from refreshing on submit
+    setShowModal(false);
   };
 
   return (
-    <form onSubmit={onSubmit}>
-      <label>
-        Select a category:
-        <select
-          value={category}
-          onChange={(event) => {
-            setCategory(event.target.value);
-          }}
-        >
-          <option value={Category.Backlog}>Backlog</option>
-          <option value={Category.InProgress}>In Progress</option>
-          <option value={Category.Done}>Done</option>
-        </select>
-      </label>
-      <input type="Submit" value="Create New Task" />
-    </form>
+    <>
+      <button
+        onClick={(e) => {
+          setShowModal(true);
+        }}
+      >
+        Create New Task
+      </button>
+      {showModal && (
+        <>
+          <div className="full-screen" />
+          <div className="modal">
+            <div className="modal-content" role="dialog">
+              <form onSubmit={onSubmit}>
+                <label>
+                  Select a category:
+                  <select
+                    value={category}
+                    onChange={(event) => {
+                      setCategory(event.target.value);
+                    }}
+                  >
+                    <option value={Category.Backlog}>Backlog</option>
+                    <option value={Category.InProgress}>In Progress</option>
+                    <option value={Category.Done}>Done</option>
+                  </select>
+                </label>
+                <br />
+                <input style={{ margin: 7 }} type="Submit" value="Submit" />
+              </form>
+              <button
+                style={{ margin: 7, float: "right" }}
+                onClick={() => {
+                  setShowModal(false);
+                }}
+              >
+                Close
+              </button>
+            </div>
+          </div>
+        </>
+      )}
+    </>
   );
 }
